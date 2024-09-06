0429d4f3b05bb5ef06fd8c1204df4ebfd2351b80
Dat Le <dat@workforceoptimizer.com>
2024-09-06 20:05:51 +0700

    [ADR-2614] create repo/ refactor

--- alns/src/main/kotlin/Main.kt
+++ alns/src/main/kotlin/Main.kt
@@ -3,24 +3,12 @@ package com.ft.aio.template.adapter.output.web.scrippt
 import com.ft.aio.template.adapter.output.web.scrippt.input.InputData
 import com.ft.aio.template.adapter.output.web.script.engine.Alns.Alns
 import java.io.File
-
-import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
-
-
-fun readJsonFromFile(filePath: String): InputData {
-    val jsonContent = File(filePath).readText()
-    return Json.decodeFromString(jsonContent)
-}
-
-fun getInput(): InputData{
-    return readJsonFromFile("data_dummy.json")
-}
+import com.ft.aio.template.adapter.output.web.scrippt.engine.PreProcess
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    var data = getInput()
+    var data = PreProcess().dataPreprocessing()
     var optimizer = Alns(data)
     println(optimizer.runAlns())
 }
--- alns/src/main/kotlin/engine/PreProcess.kt
+++ alns/src/main/kotlin/engine/PreProcess.kt
@@ -2,12 +2,10 @@ package com.ft.aio.template.adapter.output.web.scrippt.engine
 import com.ft.aio.template.adapter.output.web.scrippt.utils.ReadJSON
 import com.ft.aio.template.adapter.output.web.scrippt.input.InputData
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 
 open class PreProcess {
     fun dataPreprocessing():InputData{
-        var jsonString = ReadJSON().readJson("input/data_dummy.json")
+        var jsonString = ReadJSON().readJson("dump/data_dummy.json")
 
         val data = Gson().fromJson(jsonString, InputData:: class.java)
         return data
